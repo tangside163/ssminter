97934aec550b5c6d9d84950e4743e3280d23c7e7
tangsi <tangside163@163.com>
2016-06-12 23:04:08 +0800

    增加log4j2配置日志功能

--- my-biz/src/main/java/org/tangsi/controller/AopController.java
+++ my-biz/src/main/java/org/tangsi/controller/AopController.java
@@ -5,6 +5,8 @@ package org.tangsi.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,12 +21,18 @@ import org.tangsi.user.vo.UserVo;
 public class AopController
 {
 
+	private Logger logger = LogManager.getLogger(AopController.class);
+	
+	private Logger fileLogger = LogManager.getLogger("org.apache.log4j.biz");
+	
 	@RequestMapping("/a")
 	@ResponseBody
 	public UserVo test1(HttpServletRequest request, String name, int age)
 	{
 
-
+		logger.info("进入aopController");
+		fileLogger.info("aopcontroller写文件");
+		
 		UserVo user = new UserVo();
 		user.setName(name);
 		user.setAge(age);
